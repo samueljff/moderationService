834c8142357869f490ec056228cb9e7fdab0c7a8
samueljff <samueljosef3@gmail.com>
2025-08-23 19:43:59 -0300

    Initial project

---
+++ src/test/java/com/fonseca/algacomments/moderationService/ModerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonseca.algacomments.moderationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
